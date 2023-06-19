7cc66641779c617dcc25b01a4c3a2f869e19951f
Anish Sinha <anishsinha0128@gmail.com>
2023-06-19 19:50:36 -0400

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Jenny from "./src/components/Jenny";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar translucent={true} />
+      <Stack.Navigator initialRouteName="Business Search">
+        <Stack.Screen name="For Jenny" component={Jenny} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/components/Jenny.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Jenny = () => {
+  return (
+    <View>
+      <Text>Hi Baby. I love you.</Text>
+    </View>
+  );
+};
+
+export default Jenny;
+
+const styles = StyleSheet.create({});
